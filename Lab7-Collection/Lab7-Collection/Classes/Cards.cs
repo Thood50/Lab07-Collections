6002fc14d1363cf6cd2626739c3af3983c8d93d1
Tyler Hood <tyler.r.hood@gmail.com>
2019-01-16 12:12:43 -0800

    Initial Commit

---
+++ Lab7-Collection/Lab7-Collection/Classes/Cards.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab7_Collection.Classes
+{
+    class Cards
+    {
+    }
+}
---
+++ Lab7-Collection/Lab7-Collection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab7_Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
