5c99b62bc63b05d1504a9422d3cde68e094e00b1
Tyler Hood <tyler.r.hood@gmail.com>
2019-01-16 18:46:36 -0800

    Uh crap.....got in the groove for got to commit, cant remember what my last commit was?

--- Lab7-Collection/Lab7-Collection/Classes/Deck.cs
+++ Lab7-Collection/Lab7-Collection/Classes/Deck.cs
@@ -1,10 +1,59 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Lab7_Collection.Classes
 {
-    class Deck
+    public class Deck<T> : IEnumerable
     {
+        T[] items = new T[5];
+        int count;
+
+        public int GetCount()
+        {
+            return count;
+        }
+
+        public void AddCard(T item)
+        {
+            if (count == items.Length)
+            {
+                Array.Resize(ref items, items.Length + 1);
+            }
+            items[count++] = item;
+        }
+
+        public bool RemoveCard(int num)
+        {
+            int i;
+            for (i = 0; i < count; i++)
+            {
+                if (i >= num)
+                {
+                    items[i] = items[i + 1];
+                }
+            }
+            if (num > i)
+            {
+                return false;
+            }
+            Array.Resize(ref items, items.Length - 1);
+            count--;
+            return true;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            retrun GetEnumerator();
+        }
     }
 }
--- Lab7-Collection/Lab7-Collection/Program.cs
+++ Lab7-Collection/Lab7-Collection/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Lab7_Collection.Classes;
 
 namespace Lab7_Collection
 {
@@ -7,6 +8,121 @@ namespace Lab7_Collection
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Deck<Cards> gameDeck = CreateFullDeck();
+            Console.WriteLine("===========================================================");
         }
+
+        public static Deck<Cards> CreateFullDeck()
+        {
+            Deck<Cards> deckOfCards = new Deck<Cards>();
+            Cards AceHeart = new Cards(Number.Ace, Suites.Hearts);
+            Cards TwoHeart = new Cards(Number.Two, Suites.Hearts);
+            Cards ThreeHeart = new Cards(Number.Three, Suites.Hearts);
+            Cards FourHeart = new Cards(Number.Four, Suites.Hearts);
+            Cards FiveHeart = new Cards(Number.Five, Suites.Hearts);
+            Cards SixHeart = new Cards(Number.Six, Suites.Hearts);
+            Cards SevenHeart = new Cards(Number.Seven, Suites.Hearts);
+            Cards EightHeart = new Cards(Number.Eight, Suites.Hearts);
+            Cards NineHeart = new Cards(Number.Nine, Suites.Hearts);
+            Cards TenHeart = new Cards(Number.Ten, Suites.Hearts);
+            Cards JackHeart = new Cards(Number.Jack, Suites.Hearts);
+            Cards QueenHeart = new Cards(Number.Queen, Suites.Hearts);
+            Cards KingHeart = new Cards(Number.King, Suites.Hearts);
+            Cards AceDiamond = new Cards(Number.Ace, Suites.Diamonds);
+            Cards TwoDiamond = new Cards(Number.Two, Suites.Diamonds);
+            Cards ThreeDiamond = new Cards(Number.Three, Suites.Diamonds);
+            Cards FourDiamond = new Cards(Number.Four, Suites.Diamonds);
+            Cards FiveDiamond = new Cards(Number.Five, Suites.Diamonds);
+            Cards SixDiamond = new Cards(Number.Six, Suites.Diamonds);
+            Cards SevenDiamond = new Cards(Number.Seven, Suites.Diamonds);
+            Cards EightDiamond = new Cards(Number.Eight, Suites.Diamonds);
+            Cards NineDiamond = new Cards(Number.Nine, Suites.Diamonds);
+            Cards TenDiamond = new Cards(Number.Ten, Suites.Diamonds);
+            Cards JackDiamond = new Cards(Number.Jack, Suites.Diamonds);
+            Cards QueenDiamond = new Cards(Number.Queen, Suites.Diamonds);
+            Cards KingDiamond = new Cards(Number.King, Suites.Diamonds);
+            Cards AceClub = new Cards(Number.Ace, Suites.Clubs);
+            Cards TwoClub = new Cards(Number.Two, Suites.Clubs);
+            Cards ThreeClub = new Cards(Number.Three, Suites.Clubs);
+            Cards FourClub = new Cards(Number.Four, Suites.Clubs);
+            Cards FiveClub = new Cards(Number.Five, Suites.Clubs);
+            Cards SixClub = new Cards(Number.Six, Suites.Clubs);
+            Cards SevenClub = new Cards(Number.Seven, Suites.Clubs);
+            Cards EightClub = new Cards(Number.Eight, Suites.Clubs);
+            Cards NineClub = new Cards(Number.Nine, Suites.Clubs);
+            Cards TenClub = new Cards(Number.Ten, Suites.Clubs);
+            Cards JackClub = new Cards(Number.Jack, Suites.Clubs);
+            Cards QueenClub = new Cards(Number.Queen, Suites.Clubs);
+            Cards KingClub = new Cards(Number.King, Suites.Clubs);
+            Cards AceSpades = new Cards(Number.Ace, Suites.Spades);
+            Cards TwoSpades = new Cards(Number.Two, Suites.Spades);
+            Cards ThreeSpades = new Cards(Number.Three, Suites.Spades);
+            Cards FourSpades = new Cards(Number.Four, Suites.Spades);
+            Cards FiveSpades = new Cards(Number.Five, Suites.Spades);
+            Cards SixSpades = new Cards(Number.Six, Suites.Spades);
+            Cards SevenSpades = new Cards(Number.Seven, Suites.Spades);
+            Cards EightSpades = new Cards(Number.Eight, Suites.Spades);
+            Cards NineSpades = new Cards(Number.Nine, Suites.Spades);
+            Cards TenSpades = new Cards(Number.Ten, Suites.Spades);
+            Cards JackSpades = new Cards(Number.Jack, Suites.Spades);
+            Cards QueenSpades = new Cards(Number.Queen, Suites.Spades);
+            Cards KingSpades = new Cards(Number.King, Suites.Spades);
+
+            deckOfCards.AddCard(AceHeart);
+            deckOfCards.AddCard(TwoHeart);
+            deckOfCards.AddCard(ThreeHeart);
+            deckOfCards.AddCard(FourHeart);
+            deckOfCards.AddCard(FiveHeart);
+            deckOfCards.AddCard(SixHeart);
+            deckOfCards.AddCard(SevenHeart);
+            deckOfCards.AddCard(EightHeart);
+            deckOfCards.AddCard(NineHeart);
+            deckOfCards.AddCard(TenHeart);
+            deckOfCards.AddCard(JackHeart);
+            deckOfCards.AddCard(QueenHeart);
+            deckOfCards.AddCard(KingHeart);
+            deckOfCards.AddCard(AceDiamond);
+            deckOfCards.AddCard(TwoDiamond);
+            deckOfCards.AddCard(ThreeDiamond);
+            deckOfCards.AddCard(FourDiamond);
+            deckOfCards.AddCard(FiveDiamond);
+            deckOfCards.AddCard(SixDiamond);
+            deckOfCards.AddCard(SevenDiamond);
+            deckOfCards.AddCard(EightDiamond);
+            deckOfCards.AddCard(NineDiamond);
+            deckOfCards.AddCard(TenDiamond);
+            deckOfCards.AddCard(JackDiamond);
+            deckOfCards.AddCard(QueenDiamond);
+            deckOfCards.AddCard(KingDiamond);
+            deckOfCards.AddCard(AceClub);
+            deckOfCards.AddCard(TwoClub);
+            deckOfCards.AddCard(ThreeClub);
+            deckOfCards.AddCard(FourClub);
+            deckOfCards.AddCard(FiveClub);
+            deckOfCards.AddCard(SixClub);
+            deckOfCards.AddCard(SevenClub);
+            deckOfCards.AddCard(EightClub);
+            deckOfCards.AddCard(NineClub);
+            deckOfCards.AddCard(TenClub);
+            deckOfCards.AddCard(JackClub);
+            deckOfCards.AddCard(QueenClub);
+            deckOfCards.AddCard(KingClub);
+            deckOfCards.AddCard(AceSpades);
+            deckOfCards.AddCard(TwoSpades);
+            deckOfCards.AddCard(ThreeSpades);
+            deckOfCards.AddCard(FourSpades);
+            deckOfCards.AddCard(FiveSpades);
+            deckOfCards.AddCard(SixSpades);
+            deckOfCards.AddCard(SevenSpades);
+            deckOfCards.AddCard(EightSpades);
+            deckOfCards.AddCard(NineSpades);
+            deckOfCards.AddCard(TenSpades);
+            deckOfCards.AddCard(JackSpades);
+            deckOfCards.AddCard(QueenSpades);
+            deckOfCards.AddCard(KingSpades);
+            return deckOfCards;
+        }
+
+        public 
     }
 }
